4f7a8273eecd1b0a956c36f368b9f24ebc024529
Ben Kinard <benjaminjameskinard@gmail.com>
2023-03-20 09:50:47 -0400

    Create py files

---
+++ src/person.py
@@ -0,0 +1,16 @@
+class Person:
+    def __init__(self):
+        self.gender = None
+        self.age = None
+        self.hypertension = None
+        self.heart_disease = None
+        self.ever_married = None
+        self.work_type = None
+        self.residence_type = None
+        self.avg_glucose_level = None
+        self.bmi = None
+        self.smoking_status = None
+
+    def get_info(self):
+        # TODO: Write method here
+        return
