0ae5879a7b19261c3ea477dd808fc37df19894e6
Ben Kinard <benjaminjameskinard@gmail.com>
2023-07-27 10:05:59 -0400

    Store form info in Person object

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request
-
+from flask import Flask, flash, render_template, request
+from person import Person
 
 app = Flask(__name__)
 
@@ -11,18 +11,28 @@ def index():
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    # TODO: Handle blank form values
-    print(f"Age: {request.form['age']}")
-    print(f"Sex: {request.form['sex']}")
-    print(f"Married: {request.form['marital-status']}")
-    print(f"Heart Disease: {request.form['heart-disease']}")
-    print(f"Hypertension: {request.form['hypertension']}")
-    print(f"Work Status: {request.form['work']}")
-    print(f"Residence Type: {request.form['residence']}")
-    print(f"Avg Glucose Level: {request.form['glucose']}")
-    print(f"BMI: {request.form['bmi']}")
-    print(f"Smoking Status: {request.form['smoking']}")
-    # TODO: Create person object from form data
+    # Confirm all fields that cannot be empty are not empty
+    expected_req_fields_to_ui_map = {'age': 'Age', 'sex': 'Sex', 'work': 'Work Type', 'residence': 'Residence Type',
+                                     'smoking': 'Smoking Status'}
+    expected_fields_in_actual = [expected in request.form.keys() for expected in expected_req_fields_to_ui_map.keys()]
+    if not all(expected_fields_in_actual):
+        missing_field = list(expected_req_fields_to_ui_map.keys())[expected_fields_in_actual.index(False)]
+        flash(f"Please fill out {expected_req_fields_to_ui_map[missing_field]} field", "error")
+
+    # Create person object from form data
+    age = int(request.form['age'])
+    sex = request.form['sex']
+    is_married = 'marital-status' in request.form.keys()
+    has_heart_disease = 'heart-disease' in request.form.keys()
+    has_hypertension = 'hypertension' in request.form.keys()
+    work_type = request.form['work']
+    residence_type = request.form['residence']
+    avg_glucose_level = None if request.form['glucose'] == '' else float(request.form['glucose'])
+    bmi = None if request.form['bmi'] == '' else float(request.form['bmi'])
+    smoking_status = request.form['smoking']
+
+    person = Person(age, sex, is_married, has_heart_disease, has_hypertension, work_type, residence_type,
+                    smoking_status, avg_glucose_level, bmi)
 
     return "<h1>Prediction Landing Page</h1>"
 
--- src/person.py
+++ src/person.py
@@ -1,15 +1,17 @@
 class Person:
-    def __init__(self):
-        self.gender = None
-        self.age = None
-        self.hypertension = None
-        self.heart_disease = None
-        self.ever_married = None
-        self.work_type = None
-        self.residence_type = None
-        self.avg_glucose_level = None
-        self.bmi = None
-        self.smoking_status = None
+    def __init__(self, age: int, sex: str, is_married: bool, has_heart_disease: bool, has_hypertension: bool,
+                 work_type: str, residence_type: str, smoking_status: str, avg_glucose_level: float = None,
+                 bmi: float = None):
+        self.age: int = age
+        self.sex: str = sex
+        self.is_married: bool = is_married
+        self.has_heart_disease: bool = has_heart_disease
+        self.has_hypertension: bool = has_hypertension
+        self.work_type: str = work_type
+        self.residence_type: str = residence_type
+        self.avg_glucose_level: float = avg_glucose_level
+        self.bmi: float = bmi
+        self.smoking_status: str = smoking_status
 
     def get_info(self):
         # TODO: Write method here
