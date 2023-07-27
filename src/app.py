852f128b0304f62df2f65561165844478c7f2b5f
Ben Kinard <benjaminjameskinard@gmail.com>
2023-07-27 11:25:39 -0400

    Handle error message for missing form fields

--- src/app.py
+++ src/app.py
@@ -1,8 +1,13 @@
 import dill
-from flask import Flask, flash, render_template, request
+import uuid
+from flask import Flask, flash, redirect, render_template, request, url_for
 from person import Person
 
+# TODO: Handle wait time for prediction to load
+# TODO: Create template HTML file for prediction landing page
+# Configure app
 app = Flask(__name__)
+app.config['SECRET_KEY'] = str(uuid.uuid4())
 
 
 @app.route("/")
@@ -12,13 +17,19 @@ def index():
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    # Confirm all fields that cannot be empty are not empty
-    expected_req_fields_to_ui_map = {'age': 'Age', 'sex': 'Sex', 'work': 'Work Type', 'residence': 'Residence Type',
-                                     'smoking': 'Smoking Status'}
-    expected_fields_in_actual = [expected in request.form.keys() for expected in expected_req_fields_to_ui_map.keys()]
+    # Confirm all required fields are not empty
+    if request.form['age'] == '':
+        flash("Please fill out \"Age\" field", "error")
+        return redirect(url_for('index'))
+
+    required_drop_down_fields = {'sex': 'Sex', 'work': 'Work Type', 'residence': 'Residence Type',
+                                 'smoking': 'Smoking Status'}
+    expected_fields_in_actual = [expected in request.form.keys() for expected in required_drop_down_fields.keys()]
     if not all(expected_fields_in_actual):
-        missing_field = list(expected_req_fields_to_ui_map.keys())[expected_fields_in_actual.index(False)]
-        flash(f"Please fill out \"{expected_req_fields_to_ui_map[missing_field]}\" field", "error")
+        missing_field = list(required_drop_down_fields.keys())[expected_fields_in_actual.index(False)]
+        flash(f"Please fill out \"{required_drop_down_fields[missing_field]}\" field", "error")
+
+        return redirect(url_for('index'))
 
     # Create person object from form data
     age = int(request.form['age'])
@@ -40,7 +51,7 @@ def predict():
         model = dill.load(pickle)
     # Make prediction
     X = person.get_info()
-    is_stroke_likely = model.predict(X)
+    is_stroke_likely = model.predict(X)[0]
 
     return f"<h1>Prediction Landing Page</h1>\n<p>Likely to have a stroke: {'Yes' if is_stroke_likely else 'No'}"
 
